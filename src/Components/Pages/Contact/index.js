d9f145d7023fe1bbf0592c3a9978de4f30e7e825
Imran C <imran.chg@gmail.com>
2021-10-21 13:20:49 +0100

    Removed Insta social from Contact

--- src/Components/Pages/Contact/index.js
+++ src/Components/Pages/Contact/index.js
@@ -9,7 +9,7 @@ import {
 } from "./ContactElements";
 import Mail from "/src/Components/Images/Extras/mail.png";
 import Phone from "/src/Components/Images/Extras/phone.png";
-import Insta from "/src/Components/Images/Extras/insta.png";
+// import Insta from "/src/Components/Images/Extras/insta.png";
 import Lin from "/src/Components/Images/Extras/linkedIN.png";
 
 const Contact = () => {
@@ -26,13 +26,12 @@ const Contact = () => {
             <SocialLogo src={Phone} />
             <SocialDeets>+44 7873 505522</SocialDeets>
           </Social>
-          <Social>
-            <SocialLogo src={Insta} />
-            <SocialDeets>ic.y26</SocialDeets>
-          </Social>
+          {/*<Social>*/}
+          {/*  <SocialLogo src={Insta} />*/}
+          {/*  <SocialDeets>ic.y26</SocialDeets>*/}
+          {/*</Social>*/}
           <Social>
             <SocialLogo src={Lin} />
-            {/* <SocialDeets style={{textAlign: "left", fontSize: "12px"}}>https://www.linkedin.com/in/imran-chagani-75b4051a1/</SocialDeets> */}
             <a
               href="https://www.linkedin.com/in/imran-chagani-75b4051a1/"
               target="_blank"
